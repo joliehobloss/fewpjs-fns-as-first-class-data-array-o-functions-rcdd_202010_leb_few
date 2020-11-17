6ce550c2f7387cb11afff6b788b3e057b5bc5319
Jolie Hobloss <jolieshobloss@gmail.com>
2020-11-17 19:25:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function unleashDog(dogName,dogBreed) {
   return `Unleash ${dogName} the ${dogBreed}`;
 }
 
- const routine=[wakeDog,leashDog,walkToPark,throwFrisbeee,walkHome,unleashDog];
+ const routine=[wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog];
    
 function exerciseDog(dogName,dogBreed){
   return [...routine.forEach(exercise => exercise(dogName, dogBreed))];
